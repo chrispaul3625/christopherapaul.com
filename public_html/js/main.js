c32c2d7a78707f47aa842426a2600de7df1a94e5
Chris Paul <chrispaul3625@gmail.com>
2016-03-17 09:18:18 -0600

    messed with some css

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,57 +1,76 @@
 jQuery(document).ready(function($){
-	var	scrolling = false;
-	var contentSections = $('.cd-section'),
-		verticalNavigation = $('.cd-vertical-nav'),
-		navigationItems = verticalNavigation.find('a'),
-		navTrigger = $('.cd-nav-trigger'),
-		scrollArrow = $('.cd-scroll-down');
-
-	$(window).on('scroll', checkScroll);
-
-	//smooth scroll to the selected section
-	verticalNavigation.on('click', 'a', function(event){
+	var gallery = $('.cd-gallery'),
+		foldingPanel = $('.cd-folding-panel'),
+		mainContent = $('.cd-main');
+	/* open folding content */
+	gallery.on('click', 'a', function(event){
 		event.preventDefault();
-		smoothScroll($(this.hash));
-		verticalNavigation.removeClass('open');
+		openItemInfo($(this).attr('href'));
 	});
 
-	//smooth scroll to the second section
-	scrollArrow.on('click', function(event){
+	/* close folding content */
+	foldingPanel.on('click', '.cd-close', function(event){
 		event.preventDefault();
-		smoothScroll($(this.hash));
+		toggleContent('', false);
 	});
+	gallery.on('click', function(event){
+		/* detect click on .cd-gallery::before when the .cd-folding-panel is open */
+		if($(event.target).is('.cd-gallery') && $('.fold-is-open').length > 0 ) toggleContent('', false);
+	})
 
-	// open navigation if user clicks the .cd-nav-trigger - small devices only
-	navTrigger.on('click', function(event){
-		event.preventDefault();
-		verticalNavigation.toggleClass('open');
-	});
-
-	function checkScroll() {
-		if( !scrolling ) {
-			scrolling = true;
-			(!window.requestAnimationFrame) ? setTimeout(updateSections, 300) : window.requestAnimationFrame(updateSections);
+	function openItemInfo(url) {
+		var mq = viewportSize();
+		if( gallery.offset().top > $(window).scrollTop() && mq != 'mobile') {
+			/* if content is visible above the .cd-gallery - scroll before opening the folding panel */
+			$('body,html').animate({
+				'scrollTop': gallery.offset().top
+			}, 100, function(){
+				toggleContent(url, true);
+			});
+		} else if( gallery.offset().top + gallery.height() < $(window).scrollTop() + $(window).height()  && mq != 'mobile' ) {
+			/* if content is visible below the .cd-gallery - scroll before opening the folding panel */
+			$('body,html').animate({
+				'scrollTop': gallery.offset().top + gallery.height() - $(window).height()
+			}, 100, function(){
+				toggleContent(url, true);
+			});
+		} else {
+			toggleContent(url, true);
 		}
 	}
 
-	function updateSections() {
-		var halfWindowHeight = $(window).height()/2,
-			scrollTop = $(window).scrollTop();
-		contentSections.each(function(){
-			var section = $(this),
-				sectionId = section.attr('id'),
-				navigationItem = navigationItems.filter('[href^="#'+ sectionId +'"]');
-			( (section.offset().top - halfWindowHeight < scrollTop ) && ( section.offset().top + section.height() - halfWindowHeight > scrollTop) )
-				? navigationItem.addClass('active')
-				: navigationItem.removeClass('active');
-		});
-		scrolling = false;
+	function toggleContent(url, bool) {
+		if( bool ) {
+			/* load and show new content */
+			var foldingContent = foldingPanel.find('.cd-fold-content');
+			foldingContent.load(url+' .cd-fold-content > *', function(event){
+				setTimeout(function(){
+					$('body').addClass('overflow-hidden');
+					foldingPanel.addClass('is-open');
+					mainContent.addClass('fold-is-open');
+				}, 100);
+
+			});
+		} else {
+			/* close the folding panel */
+			var mq = viewportSize();
+			foldingPanel.removeClass('is-open');
+			mainContent.removeClass('fold-is-open');
+
+			(mq == 'mobile' || $('.no-csstransitions').length > 0 )
+				/* according to the mq, immediately remove the .overflow-hidden or wait for the end of the animation */
+				? $('body').removeClass('overflow-hidden')
+
+				: mainContent.find('.cd-item').eq(0).one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+				$('body').removeClass('overflow-hidden');
+				mainContent.find('.cd-item').eq(0).off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+			});
+		}
+
 	}
 
-	function smoothScroll(target) {
-		$('body,html').animate(
-			{'scrollTop':target.offset().top},
-			300
-		);
+	function viewportSize() {
+		/* retrieve the content value of .cd-main::before to check the actua mq */
+		return window.getComputedStyle(document.querySelector('.cd-main'), '::before').getPropertyValue('content').replace(/"/g, "").replace(/'/g, "");
 	}
 });
